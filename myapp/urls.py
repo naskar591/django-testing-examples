88bdafe2b588a884b521e230d5d1251a7cef1881
naskar950 <naskarsubarna950@gmail.com>
2024-10-29 10:02:23 +0530

    code refactor

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import url
-
+from django.urls import re_path
 from . import views
 
 app_name = 'myapp'
 urlpatterns = [
-    url(r'^mypath/$', views.MyView.as_view(), name='myview'),
-    url(r'^create/$', views.MyView.as_view(), name='mycreateview'),
-    url(r'^(?P<pk>[0-9]+)/update/$', views.MyUpdateView.as_view(), name='myupdateview'),
-
+    re_path(r'^mypath/$', views.MyView.as_view(), name='myview'),
+    re_path(r'^create/$', views.MyView.as_view(), name='mycreateview'),
+    re_path(r'^(?P<pk>[0-9]+)/update/$', views.MyUpdateView.as_view(), name='myupdateview'),
 ]
